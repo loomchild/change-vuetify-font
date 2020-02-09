8edf2ce4963f01eace773c31a2793dbfb9198a6b
Jarek Lipski <loomchild@loomchild.net>
2020-02-09 18:01:24 +0100

    remove button

--- pages/index.vue
+++ pages/index.vue
@@ -8,9 +8,6 @@
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id massa viverra, aliquet augue nec, rhoncus turpis. Nam vitae ultricies enim. Aenean non pellentesque mauris, quis porta ligula. Mauris vulputate urna sit amet arcu scelerisque finibus. Quisque non tempor massa, vitae venenatis urna. Aenean at eleifend ex. Integer sollicitudin ex turpis, et ullamcorper sapien pellentesque sit amet. Vestibulum a ultricies ante. Sed posuere non nunc at tempus. Curabitur malesuada scelerisque tortor, eget suscipit urna. Praesent quis tellus in tortor condimentum ultricies non ut odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
         </p>
-        <v-btn>
-          It works!
-        </v-btn>
       </v-col>
     </v-row>
   </v-container>
