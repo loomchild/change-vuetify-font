473ccc2d24e1ffa94cef9ea87717e0935f30f553
Jarek Lipski <loomchild@loomchild.net>
2020-02-09 15:51:45 +0100

    shorten example paragraph

--- pages/index.vue
+++ pages/index.vue
@@ -7,8 +7,6 @@
         </h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id massa viverra, aliquet augue nec, rhoncus turpis. Nam vitae ultricies enim. Aenean non pellentesque mauris, quis porta ligula. Mauris vulputate urna sit amet arcu scelerisque finibus. Quisque non tempor massa, vitae venenatis urna. Aenean at eleifend ex. Integer sollicitudin ex turpis, et ullamcorper sapien pellentesque sit amet. Vestibulum a ultricies ante. Sed posuere non nunc at tempus. Curabitur malesuada scelerisque tortor, eget suscipit urna. Praesent quis tellus in tortor condimentum ultricies non ut odio. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-
-          Integer tortor lacus, tristique quis placerat a, iaculis et dui. Curabitur pulvinar faucibus turpis, bibendum maximus velit malesuada non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Curabitur aliquam, libero lobortis consequat fermentum, eros dolor gravida lacus, et mollis turpis diam quis massa. Praesent eget libero sit amet mauris tristique semper nec id velit. Etiam non mattis sem. Nam malesuada urna sed finibus malesuada. Nulla elementum nulla at laoreet aliquet. Nulla dictum lorem ut enim cursus, non tempus massa molestie. Donec sem metus, molestie ut volutpat non, tristique egestas mauris. Phasellus sit amet eros ultricies, malesuada dolor id, dapibus nunc. Suspendisse tincidunt, enim a pellentesque laoreet, eros nunc aliquet libero, eget dapibus ante diam sit amet dolor. Mauris scelerisque leo ligula, vel ultrices lectus varius eu. Aenean nec ipsum ipsum. Sed laoreet odio vel lorem consequat tincidunt. Suspendisse euismod porta diam.
         </p>
         <v-btn>
           It works!
